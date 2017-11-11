fix softmax

SoftMax.py
@@ -3,15 +3,18 @@ import numpy as np
 from Agent import Agent
 
 class SoftMax(Agent):
-  def __init__(self, principals, priors=None, alpha=-3):
+  def __init__(self, principals, priors=None, alpha=0.2):
     self.alpha = alpha
     super(SoftMax, self).__init__(principals, priors)
 
   def selectPrincipal(self):
     scores = self.informationSet.getScores()
-    for (principal, score) in scores:
-      if rand.random() < (1-np.exp(score * self.alpha)): #this seems wrong...
-        return (principal, self.principals[maxPrincipal])
-
-  def tieBreak(self, items):
-    return rand.choice(items)
+    total = sum([np.exp(score * self.alpha) for score in scores.values()])
+    probs = { principal: np.exp(score * self.alpha) / total for (principal, score) in scores.iteritems() }
+    
+    threshold = rand.random()
+    cumProb = 0.0
+    for (principal, prob) in probs.iteritems():
+      cumProb += prob
+      if cumProb > threshold:
+        return (principal, self.principals[principal])