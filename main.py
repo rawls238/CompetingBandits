50a36d532e9c3c3859d3840ac5aff57b8601196b
Kevin Liu <kevin91liu@gmail.com>
2017-11-26 18:34:17 -0500

    allow principal 1 to have different priors from principal 2

--- main.py
+++ main.py
@@ -29,33 +29,39 @@ import pickle
 
 
 K = 10
-T = 2000.0
+T = 5000.0
 
 # misspecified in the sense that the principal's initial belief (priors) is wrong compared to
 # the real distribution
-MISSPECIFIED_PRIOR = [beta(0.45, 0.55) for k in xrange(K)]
+# MISSPECIFIED_PRIOR = [beta(0.45, 0.55) for k in xrange(K)]
 
-INITIAL_PRINCIPAL_PRIORS = [beta(0.55, 0.45) for k in xrange(K)]
+# INITIAL_PRINCIPAL_PRIORS = [beta(0.55, 0.45) for k in xrange(K)]
 
-REAL_DISTRIBUTIONS = [bernoulli(0.55) for k in xrange(K)]
+REAL_DISTRIBUTIONS = [bernoulli(0.45) for k in xrange(K)]
 # now, overwrite the first distribution so that it's better than all the others
 REAL_DISTRIBUTIONS[0] = bernoulli(0.8)
+REAL_DISTRIBUTIONS[1] = bernoulli(0.55)
+
+PRINCIPAL1PRIORS = [beta(0.45, 0.55) for k in xrange(K)]
+PRINCIPAL1PRIORS[1] = beta(0.55, 0.45)
+
+PRINCIPAL2PRIORS = [beta(0.6, 0.4) for k in xrange(K)]
 
 # realDistributions - the true distribution of the arms
 # principalPriors - the priors the principals have over the arms
 # agentPriors - the priors the agents have for the distribution of rewards from each principal
-
 def simulate(principalAlg1, principalAlg2, agentAlg, 
   realDistributions=REAL_DISTRIBUTIONS,
-  principalPriors=MISSPECIFIED_PRIOR,
-  agentPriors={ 'principal1': beta(0.6, 0.4), 'principal2': beta(0.6, 0.4) }):
+  principal1Priors=PRINCIPAL1PRIORS,
+  principal2Priors=PRINCIPAL2PRIORS,
+  agentPriors={ 'principal1': beta(0.6, 0.4), 'principal2': beta(0.5, 0.5) }):
 
   banditProblemInstance = BanditProblemInstance(K, T, realDistributions)
 
 
   # instantiate 2 principals (who are of some subclass of BanditAlgorithm)
-  principal1 = principalAlg1(banditProblemInstance, principalPriors)
-  principal2 = principalAlg2(banditProblemInstance, principalPriors)
+  principal1 = principalAlg1(banditProblemInstance, principal1Priors)
+  principal2 = principalAlg2(banditProblemInstance, principal2Priors)
 
   principals = { 'principal1': principal1, 'principal2': principal2 }
   agents = agentAlg(principals, K, agentPriors)
