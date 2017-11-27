67eb2fbbc0d4511e5fb63dbed1d16097a04f7977
Kevin Liu <kevin91liu@gmail.com>
2017-11-26 23:39:19 -0500

    improvement to HardMaxWithRandom and SoftMax
    HardMaxWithRandom: make meaning of “epsilon” line up with the meaning
    in SoftMax
    SoftMax: implement logistic function

--- HardMaxWithRandom.py
+++ HardMaxWithRandom.py
@@ -2,12 +2,14 @@ import random as rand
 from HardMax import HardMax
 
 class HardMaxWithRandom(HardMax):
-  def __init__(self, principals, K, priors=None, epsilon=0.05):
+  # epsilon: the baseline probablility that each principal will be picked with. must be in (0, 0.5).
+  # if epsilon=0.1, for example, then 20% of the time, the agent picks randomly between the 2 principals
+  def __init__(self, principals, K, priors=None, epsilon=0.1):
     super(HardMaxWithRandom, self).__init__(principals, K, priors)
     self.epsilon = epsilon
 
   def selectPrincipal(self):
-    if rand.random() > self.epsilon:
+    if rand.random() > self.epsilon * 2:
       return super(HardMaxWithRandom, self).selectPrincipal()
     else:
       principal = self.informationSet.getRandPrincipal()
--- SoftMax.py
+++ SoftMax.py
@@ -3,20 +3,38 @@ import numpy as np
 from Agent import Agent
 
 
-DEFAULT_ALPHA = .2
+DEFAULT_ALPHA = 10
 class SoftMax(Agent):
-  def __init__(self, principals, K, priors=None, alpha=DEFAULT_ALPHA):
+  # epsilon: must be in (0, 0.5). baseline probability that each principal gets picked
+  def __init__(self, principals, K, priors=None, alpha=DEFAULT_ALPHA, epsilon=0.1):
     super(SoftMax, self).__init__(principals, K, priors)
     self.alpha = alpha
+    self.epsilon = epsilon
 
+  # def selectPrincipal(self):
+  #   scores = self.informationSet.getScores()
+  #   total = sum([np.exp(score * self.alpha) for score in scores.values()])
+  #   probs = { principal: np.exp(score * self.alpha) / total for (principal, score) in scores.iteritems() }
+    
+  #   threshold = rand.random()
+  #   cumProb = 0.0
+  #   for (principal, prob) in probs.iteritems():
+  #     cumProb += prob
+  #     if cumProb > threshold:
+  #       return (principal, self.principals[principal])
+
+  # https://en.wikipedia.org/wiki/Logistic_function
+  # alpha corresponds to "k" in the wikipedia article (it controls the steepness of the curve)
   def selectPrincipal(self):
     scores = self.informationSet.getScores()
-    total = sum([np.exp(score * self.alpha) for score in scores.values()])
-    probs = { principal: np.exp(score * self.alpha) / total for (principal, score) in scores.iteritems() }
-    
-    threshold = rand.random()
-    cumProb = 0.0
-    for (principal, prob) in probs.iteritems():
-      cumProb += prob
-      if cumProb > threshold:
-        return (principal, self.principals[principal])
+    x = scores['principal1'] - scores['principal2'] # how much better 1 is than 2
+
+    y = self.epsilon + ((1 - 2 * self.epsilon) / (1 + np.exp(-self.alpha * x)))
+
+    r = rand.random()
+
+    if r < y:
+      # then choose principal 1
+      return ('principal1', self.principals['principal1'])
+    else:
+      return ('principal2', self.principals['principal2'])
