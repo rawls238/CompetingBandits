a3c447fd9e74f8f3d34ec49d10d04c1cc8076c38
Guy Aridor <garidor@gmail.com>
2018-01-29 13:18:41 -0500

    add prelim report

--- analysis_code/plots.R
+++ analysis_code/plots.R
@@ -3,9 +3,8 @@ library("dplyr")
 library("reshape")
 
 # Three datasets that currently exist are:
-# preliminary_plots.csv includes more distributions but the resulting reward means are only averaged over fifty rounds.
-# preliminary_plots_2.csv was run with N=250, T=1000
-# preliminary_plots_3.csv was run with N=250, T=5000
+# preliminary_plots.csv was run with N=250, T=6000
+# preliminary_plots_2.csv includes the Bayesian Dynamic Epsilon Greedy runs
 
 WORKING_PATH <- "/Users/garidor/Desktop/bandits-rl-project"
 
@@ -19,11 +18,11 @@ dists <- as.list(unique(dat['Distribution']))$Distribution
 
 filter_by_dist_and_plot <- function(dist) {
   d <- filter(dat, Distribution == dist)
-  title <- paste("Reward Trajectory for", dist, "all")
-  q <- ggplot(data=d, aes(x=t, y=Instantaneous.Reward.Mean, colour=Algorithm)) + ggtitle(title) + ylab("Cumulative Mean Reward") + xlab("time") +
+  title <- paste("Reward Trajectory for", dist, "inst")
+  q <- ggplot(data=d, aes(x=t, y=Instantaneous.Reward.Mean, colour=Algorithm)) + ggtitle(title) + ylab("Instantaneous Mean Reward") + xlab("time") +
     #geom_path() # just plot the raw trajectory
     geom_smooth(method="loess") #+ smooths the trajectory 
-    #geom_errorbar(aes(ymin=Reward.Mean-1.96*Reward.Std, ymax=Reward.Mean+1.96*Reward.Std))
+    #geom_errorbar(aes(ymin=Instantaneous.Reward.Mean-1.96*Instantaneous.Reward.Std, ymax=Instantaneous.Reward.Mean+1.96*Instantaneous.Reward.Std))
   ggsave(width=8, height=8, dpi=300, filename=paste(WORKING_PATH, "/results/", title, ".pdf", sep=""), plot=q)
   
   return(d)
@@ -31,5 +30,14 @@ filter_by_dist_and_plot <- function(dist) {
 
 lapply(dists, filter_by_dist_and_plot)
 
+find_average_std_dev <- function() {
+  lapply(dists, function(dist) {
+    return(lapply(algs, function(alg) {
+      d <- filter(dat, Distribution == dist)
+      d <- filter(d, Algorithm == alg)
+      return(mean(d['Instantaneous.Reward.Std'][[1]]))
+    })) 
+  })
+}
 
-
+#print(find_average_std_dev())
