ec1052889ea7e28f70c8a17b9a2c071b7195bf4b
Kevin Liu <kevin91liu@gmail.com>
2018-01-31 16:32:02 -0500

    separate out class into its own file

--- lib/bandit/NonBayesianEpsilonGreedy.py
+++ lib/bandit/NonBayesianEpsilonGreedy.py
@@ -3,23 +3,21 @@ from BanditAlgorithm import BanditAlgorithm
 import numpy as np
 import random as rand
 
-# shouldn't this be a subclass of DynamicGreedy?
-class DynamicEpsilonGreedy(BanditAlgorithm):
+
+class NonBayesianEpsilonGreedy(BanditAlgorithm):
   def __init__(self, banditProblemInstance, priors, epsilon=0.05, dynamicEpsilon=False):
-    super(DynamicEpsilonGreedy, self).__init__(banditProblemInstance, priors)
+    super(NonBayesianEpsilonGreedy, self).__init__(banditProblemInstance, priors)
     self.epsilon = epsilon
     self.dynamicEpsilon = dynamicEpsilon
 
   @staticmethod
   def shorthand():
-    return 'DEG'
+    return 'NBDEG'
 
   def pickAnArm(self, t):
     if self.dynamicEpsilon:
-      self.epsilon = 1.0 / (t + 1)
-    chosenArm = np.argmax([p.mean() for p in self.posteriors])
+      self.epsilon = (t + 1)**(-1/3)
     if rand.random() > self.epsilon:
-      return chosenArm
+      return np.argmax([mean for mean in self.empiricalMeans])
     else:
       return rand.choice(range(self.banditProblemInstance.K))
-
