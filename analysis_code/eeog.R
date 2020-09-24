a0a2078454dfe14d0521659ac89c65f381dfdf9c
Guy Aridor <garidor@gmail.com>
2020-09-24 14:51:34 -0400

    add EoG values for everything

--- analysis_code/eeog.R
+++ analysis_code/eeog.R
@@ -14,8 +14,8 @@ concise_alg_rep <- function(alg) {
   }
 }
 
-WORKING_PATH <- "/Users/garidor/Desktop/CompetingBandits/results/tournament_raw_results/"
-f <- "tournament_experiment_longer_ws_raw.csv"
+WORKING_PATH <- "/Users/guyaridor/Dropbox/bandits_results/tournament_raw_results/"
+f <- "tournament_experiment_full_welfare_calc_raw.csv"
 dat <- read.csv(file=paste(WORKING_PATH, f, sep=""))
 dat <- filter(dat, Time.Horizon == 2000)
 priors <- as.list(unique(dat['Prior']))$Prior
@@ -25,9 +25,12 @@ warm_starts <- c(20, 250, 500)
 algs <- c("ThompsonSampling", "DynamicEpsilonGreedy", "DynamicGreedy")
 
 for (prior in priors) {
-  results <- matrix(nrow=3, ncol=length(warm_starts))
-  rownames(results) <- c("TS vs DG", "TS vs DEG", "DG vs DEG")
-  colnames(results) <- lapply(warm_starts, function (start) { return(paste("$T_0$ =", start))})
+  results_market_shares <- matrix(nrow=3, ncol=length(warm_starts))
+  results_eeog <- matrix(nrow=3, ncol=length(warm_starts))
+  rownames(results_market_shares) <- c("TS vs DG", "TS vs DEG", "DG vs DEG")
+  rownames(results_eeog) <- c("TS vs DG", "TS vs DEG", "DG vs DEG")
+  colnames(results_market_shares) <- lapply(warm_starts, function (start) { return(paste("$T_0$ =", start))})
+  colnames(results_eeog) <- lapply(warm_starts, function (start) { return(paste("$T_0$ =", start))})
   for (i in 1:length(warm_starts)) {
     warm_start = warm_starts[i]
     for (k in 1:length(algs)) {
@@ -41,20 +44,25 @@ for (prior in priors) {
         cin <- signif(test$conf.int[2] - mean(share), digits=1)
         eeog_mean <- signif(mean(filtered_dat$EEOG), digits=2)
         eeog_summary <- summary(filtered_dat$EEOG)
-        res_text <- paste("\\makecell{\\textbf{",signif(mean(share), digits=2), "} $\\pm$", cin, "\\\\ eeog \\\\ avg: ", eeog_mean, "\\\\ med: ", eeog_summary["Median"], "}" , sep="")
+        res_text <- paste("\\makecell{\\textbf{",signif(mean(share), digits=2), "} $\\pm$", cin, "}" , sep="")
+        res_eeog <- paste(eeog_mean, " (", eeog_summary["Median"], ") ", sep="")
         if (p1alg == "ThompsonSampling" && p2alg == "DynamicGreedy") {
-          results[1, i] <- res_text
+          results_market_shares[1, i] <- res_text
+          results_eeog[1, i] <- res_eeog
         } else if (p1alg == "ThompsonSampling" && p2alg == "DynamicEpsilonGreedy") {
-          results[2, i] <- res_text
+          results_market_shares[2, i] <- res_text
+          results_eeog[2, i] <- res_eeog
         } else if (p1alg == "DynamicGreedy" && p2alg == "DynamicEpsilonGreedy") {
-          results[3, i] <- res_text
+          results_market_shares[3, i] <- res_text
+          results_eeog[3, i] <- res_eeog
         }
       }
     }
   }
-  tab <-xtable(results, caption=paste("Duopoly Experiment", prior), type="latex")
+  #tab <-xtable(results_market_shares, caption=paste("Duopoly Market Shares Experiment", prior), type="latex")
+  #print(tab)
+  tab <-xtable(results_eeog, caption=paste("Duopoly EEOG Experiment", prior), type="latex")
   print(tab)
-  
 }
 
 
